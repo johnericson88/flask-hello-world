1f03f3c0001bb1c076cb0af2b4b38ecc5d4888ac
johncus <cusickjc@gmail.com>
2024-08-05 15:35:02 -0400

    layout html, favicon

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Hi'
+    return render_template("hello_world.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
